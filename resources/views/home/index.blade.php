95d64c5aa4bde505ddad182f0d76b50fcf8ef0c3
Allan Barcelos <allanbarcelosmg@gmail.com>
2018-12-11 12:04:29 -0600

    home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,19 +3,35 @@
 @section('content')
 <!--{{ Breadcrumbs::render('homeIndex') }}-->
 <div class="row">
-  <div class="col-md-6">
-    <h3>Última edição</h3>
-    <hr />
-    <div id="the-canvas"></div>
-    <button type="button" name="button" class="btn btn-lg btn-block btn-success ">Ler Agora</button>
-  </div>
+    <div class="col-md-6">
+        <h3>Última edição</h3>
+        <hr />
+        <div id="the-canvas"></div>
+        <button type="button" name="button" class="btn btn-lg btn-block btn-success ">Ler Agora</button>
+    </div>
 
-  <div class="col-md-6">
-    <h3>Última edição</h3>
-    <hr />
+    <div class="col-md-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+                <i class="fa fa-chevron-up"></i>
+            </h3>
+          </div>
+          <div class="panel-body">
+              <div class="row">
+                <div class="col-md-4 offset-md-1">
+                  <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
+                </div>
+                <div class="col-md-4 offset-md-1">
+                    <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
+                </div>
+              </div>
+          </div>
+          <div class="panel-footer">
 
-
-  </div>
+          </div>
+        </div>
+    </div>
 
 
 </div>
@@ -27,9 +43,24 @@
 
 
 @section('scripts')
-<script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
 <script type="text/javascript">
 
+$('.carousel .vertical .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=1;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
 
 </script>
 @endsection
