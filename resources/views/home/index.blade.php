e261c868c9866ef5a0ebbf436d5ac60523ee77f2
Allan Barcelos <allanbarcelosmg@gmail.com>
2018-12-12 07:42:35 -0200

    layout base

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,44 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<!--{{ Breadcrumbs::render('homeIndex') }}-->
 <div class="row">
-    <div class="col-md-6">
-        <h3>Última edição</h3>
-        <hr />
-        <div id="the-canvas"></div>
-        <button type="button" name="button" class="btn btn-lg btn-block btn-success ">Ler Agora</button>
+  <div class="col-md-7">
+    <h3>Última edição</h3>
+    <hr />
+    <div id="the-canvas"></div>
+    <button type="button" name="button" class="btn btn-lg btn-block btn-success ">Ler Agora</button>
+  </div>
+
+  <div class="col-md-5 bg-secondary">
+    <h3 class="text-center"><i class="fa fa-chevron-up"></i></h3>
+    <hr />
+    <div class="row">
+      <div class="col-md-5 offset-md-1 text-center">
+        <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
+      </div>
+      <div class="col-md-5 text-center">
+        <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
+      </div>
     </div>
-
-    <div class="col-md-6">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">
-                <i class="fa fa-chevron-up"></i>
-            </h3>
-          </div>
-          <div class="panel-body">
-              <div class="row">
-                <div class="col-md-4 offset-md-1">
-                  <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
-                </div>
-                <div class="col-md-4 offset-md-1">
-                    <img src="https://via.placeholder.com/210x297" class="img-fluid img-thumbnail" alt="">
-                </div>
-              </div>
-          </div>
-          <div class="panel-footer">
-
-          </div>
-        </div>
-    </div>
-
-
+    <div class="row">&nbsp;</div>
+  </div>
 </div>
-
-<div id="the-canvas"></div>
-
-
 @endsection
 
 
@@ -46,20 +30,20 @@
 <script type="text/javascript">
 
 $('.carousel .vertical .item').each(function(){
-    var next = $(this).next();
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=1;i<2;i++) {
+    next=next.next();
     if (!next.length) {
-        next = $(this).siblings(':first');
+      next = $(this).siblings(':first');
     }
+
     next.children(':first-child').clone().appendTo($(this));
-
-    for (var i=1;i<2;i++) {
-        next=next.next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-
-        next.children(':first-child').clone().appendTo($(this));
-    }
+  }
 });
 
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,112 +1,125 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="//fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="//fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/flipbook.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/flipbook.css') }}" rel="stylesheet">
 
-    @yield('styles')
+  @yield('styles')
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('adminIndex')}}">
-                                Dashboard <i class="fas fa-desktop"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                {{ Auth::user()->name }} <i class="fas fa-cog"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }} <i class="fas fa-sign-out-alt"></i></a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </li>
-                        @endguest
-                    </ul>
+<body class="bg-white">
+  <div id="app">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+      <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto"></ul>
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @else
+
+            @if(Auth::user()->authorizeRoles(['administrator']))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('adminIndex')}}">
+                Dashboard <i class="fas fa-desktop"></i>
+              </a>
+            @endif
+
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                {{ Auth::user()->name }} <i class="fas fa-cog"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              {{ __('Logout') }} <i class="fas fa-sign-out-alt"></i></a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </li>
+            @endguest
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    @auth
+    <nav class="navbar navbar-light navbar-secondary bg-light">
+      <div class="container">
+          <div class="col-md-7">
+            <form class="form-inline">
+              <div class="input-group col-md-8">
+                <input type="text" class="form-control border-right-0" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                <div class="input-group-append">
+                  <span class="input-group-text bg-white" id="inputGroup-sizing-default">
+                    <i class="fa fa-search"></i>
+                  </span>
                 </div>
+              </div>
+
+            </form>
+          </div>
+          <div class="col-md-5 dropdown-full float-right">
+            <div class="dropdown dropdown-full">
+              <button class="btn btn-secondary btn-block dropdown-toggle"
+                      type="button" id="dropdownMenuButton"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false">
+                <span>Dropdown button</span>
+                <i class="fa fa-chevron-down"></i>
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
             </div>
-        </nav>
-
-
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
-
-                        <form class="form-inline">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-
-
-
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
-
-
-            </div>
-
-        </nav>
+          </div>
+      </div>
+    </nav>
+    @endauth
 
 
 
 
+    <main class="container">
+      @yield('content')
+    </main>
+  </div>
 
-        <main class="py-4 container">
-            @yield('content')
-        </main>
-    </div>
-
-    <!-- Scripts -->
-    <script src="//code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    @yield('scripts')
+  <!-- Scripts -->
+  <script src="//code.jquery.com/jquery-3.3.1.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  @yield('scripts')
 </body>
 </html>
